e7e9a4dd684ca3b414f0478242b8e5a9d5550f59
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2024-07-26 15:00:42 -0300

    072624 aula0207

--- js/principal.js
+++ js/principal.js
@@ -11,6 +11,23 @@ var altura = tdAltura.textContent;
 
 var tdImc = paciente.querySelector(".info-imc");
 
-var imc = peso / (altura * altura);
+var pesoEhValido = true;
+var alturaEhValida = true;
 
-tdImc.textContent = imc;
+if(peso <= 0 || peso >= 250) {
+    console.log("Peso inválido!");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido!";
+}
+
+if (altura <= 0 || altura >= 2.50) {
+    console.log("Altura inválida!");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida!"
+}
+
+if (alturaEhValida && pesoEhValido) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
+
