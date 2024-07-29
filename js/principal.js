9630a2d93953aae291697d00436bd5cf5e4c9081
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2024-07-29 14:35:48 -0300

    072924 aula0401

--- js/principal.js
+++ js/principal.js
@@ -3,7 +3,6 @@ titulo.textContent = "Aparecida Nutricionista";
 
 var pacientes = document.querySelectorAll(".paciente");
 
-
 for(var i = 0; i < pacientes.length; i++) {
 
     var paciente = pacientes[i];
@@ -38,3 +37,8 @@ for(var i = 0; i < pacientes.length; i++) {
         tdImc.textContent = imc.toFixed(2);
     }
 }
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(){
+    console.log("Oi, cliquei no botão!");
+})
